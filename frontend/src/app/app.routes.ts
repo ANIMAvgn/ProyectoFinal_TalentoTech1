07e5e713b3f345c19f0d3c9b6dc5af51e7cc60b0
Gabriela Aguilar <gabrielaaguiv@gmail.com>
2026-03-06 23:36:19 -0500

    Update Landing Page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { Perfil } from './pages/perfil/perfil';
 import { Professionals } from './pages/professionals/professionals';
+import { LandingComponent } from './pages/landing/landing';
 import { AppointmentBooking } from './pages/appointment-booking/appointment-booking';
 
+
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: LandingComponent },   // landing page
+  { path: 'login', component: LoginComponent },
   { path: 'professionals', component: Professionals },
   { path: 'booking/:id', component: AppointmentBooking },
   { path: 'perfil', component: Perfil }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
